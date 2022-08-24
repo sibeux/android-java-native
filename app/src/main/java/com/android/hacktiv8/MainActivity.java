3956fbe3acfd96707ab51cade125b304644956e0
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-08-24 23:33:19 +0700

    android java native

---
+++ app/src/main/java/com/android/hacktiv8/MainActivity.java
@@ -0,0 +1,64 @@
+package com.android.hacktiv8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    TextView form;
+    public static final String myPreference = "pref";
+    public static final String Form = "former";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        form = findViewById(R.id.formText);
+        sharedPreferences = getSharedPreferences(myPreference,
+                Context.MODE_PRIVATE);
+        if (sharedPreferences.contains(Form)){
+            form.setText(sharedPreferences.getString(Form,""));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart()",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop()",Toast.LENGTH_LONG).show();
+//         save isi dari input
+        String f = form.getText().toString();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(Form, f);
+        editor.apply();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause()",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume()",Toast.LENGTH_LONG).show();
+        // call data yg saved dan showing
+        form = findViewById(R.id.formText);
+        sharedPreferences = getSharedPreferences(myPreference,
+                Context.MODE_PRIVATE);
+        if (sharedPreferences.contains(Form)){
+            form.setText(sharedPreferences.getString(Form,""));
+        }
+    }
+}
