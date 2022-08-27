ed313b9b9bd0bc89ccd1e0f6c4ba6b754fd2da5b
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-08-28 02:14:58 +0700

    android buttons

---
+++ app/src/main/java/com/belajar/android/Buttons.java
@@ -0,0 +1,37 @@
+package com.belajar.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class Buttons extends AppCompatActivity {
+
+    View button;
+    View button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+
+        button = findViewById(R.id.click);
+        button2 = findViewById(R.id.unclick);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setBackgroundColor(Color.CYAN);
+                button2.setBackgroundColor(Color.BLUE);
+                button2.setClickable(true);
+                button2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        button.setBackgroundColor(Color.GREEN);
+                    }
+                });
+            }
+        });
+    }
+}
