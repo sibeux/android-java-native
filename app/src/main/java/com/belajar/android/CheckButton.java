6ba0b55d174c223c8d0306495a91b8e70fe94043
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-08-29 01:24:00 +0700

    android CheckButton

--- app/src/main/java/com/belajar/android/CheckButton.java
+++ app/src/main/java/com/belajar/android/CheckButton.java
@@ -6,24 +6,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.CheckBox;
 
-public class ImagesView extends AppCompatActivity {
+public class CheckButton extends AppCompatActivity {
 
     TextView name;
     TextView result;
     View button;
     ImageView image;
     String username;
+    CheckBox male,female;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_images_view);
+        setContentView(R.layout.activity_check_button);
 
         name = findViewById(R.id.name);
         result = findViewById(R.id.result);
         button = findViewById(R.id.button);
         image = findViewById(R.id.imagesView);
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (male.isChecked()){
+                    result.setText("Male");
+                    female.setChecked(false);
+                } else{
+                    result.setText("What tf your gender?");
+                }
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (female.isChecked()){
+                    result.setText("Female");
+                    male.setChecked(false);
+                } else{
+                    result.setText("What tf your gender?");
+                }
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
