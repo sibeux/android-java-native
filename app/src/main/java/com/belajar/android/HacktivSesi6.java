d2cdcf3c7995e160de54fe7d84665e1c04280966
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-08-31 02:56:12 +0700

    tugas java sesi 6 intent app dan put extra

---
+++ app/src/main/java/com/belajar/android/HacktivSesi6.java
@@ -0,0 +1,69 @@
+package com.belajar.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HacktivSesi6 extends AppCompatActivity {
+
+    TextView namaLengkap, alamat, umur, universitas, jurusan;
+    Button submit;
+    RadioGroup kelamin;
+    RadioButton pria, wanita;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hacktiv_sesi6);
+
+        namaLengkap = findViewById(R.id.namaLengkap);
+        alamat = findViewById(R.id.alamat);
+        umur = findViewById(R.id.umur);
+        universitas = findViewById(R.id.universitas);
+        jurusan = findViewById(R.id.jurusan);
+        submit = findViewById(R.id.submit_area);
+        kelamin = findViewById(R.id.kelamin);
+        pria = findViewById(R.id.radioMale);
+        wanita = findViewById(R.id.radioFemale);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (namaLengkap.getText().toString().length()==0){
+                    namaLengkap.setError("Nama tidak boleh kosong!");
+                }else if (alamat.getText().toString().length()==0){
+                    alamat.setError("Alamat tidak boleh kosong!");
+                }else if (umur.getText().toString().length()==0){
+                    umur.setError("Umur tidak boleh kosong!");
+                }else if (universitas.getText().toString().length()==0){
+                    universitas.setError("Universitas tidak boleh kosong!");
+                }else if (jurusan.getText().toString().length()==0){
+                    jurusan.setError("Jurusan tidak boleh kosong!");
+                }else if (kelamin.getCheckedRadioButtonId() == -1){
+                    pria.setError("Pilih salah satu!");
+                    wanita.setError("pilih salah satu!");
+                } else{
+                    pria.setError(null);
+                    wanita.setError(null);
+                    Toast.makeText(getApplicationContext(), "Registrasi Berhasil!",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(String.valueOf(HacktivSesi6_view.class));
+                    intent.putExtra("namaLengkap",namaLengkap.getText().toString());
+                    intent.putExtra("alamat",alamat.getText().toString());
+                    intent.putExtra("umur",umur.getText().toString());
+                    intent.putExtra("universitas",universitas.getText().toString());
+                    intent.putExtra("jurusan",jurusan.getText().toString());
+                    String kelamin = pria.isChecked() ? "Laki-laki" : "Perempuan";
+                    intent.putExtra("kelamin",kelamin);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/belajar/android/HacktivSesi6_view.java
@@ -0,0 +1,14 @@
+package com.belajar.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HacktivSesi6_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hacktiv_sesi6_view);
+    }
+}
