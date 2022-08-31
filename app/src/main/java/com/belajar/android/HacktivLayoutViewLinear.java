72b002bac53aec142a5e087e26ecebfb469ff02f
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-09-01 00:33:06 +0700

    intent app java

--- app/src/main/java/com/belajar/android/HacktivLayoutViewLinear.java
+++ app/src/main/java/com/belajar/android/HacktivLayoutViewLinear.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HacktivLayoutView extends AppCompatActivity {
+public class HacktivLayoutViewLinear extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
