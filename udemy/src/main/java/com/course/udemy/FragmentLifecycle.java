0967c382c6582c90cd388596e3826064f0176115
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-09-09 20:29:08 +0700

    fragment & lifecycle

---
+++ udemy/src/main/java/com/course/udemy/FragmentLifecycle.java
@@ -0,0 +1,14 @@
+package com.course.udemy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentLifecycle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_lifecycle);
+    }
+}
